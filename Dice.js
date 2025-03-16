9a704e1ee61f3b39c4f5e5f44ef7297b6a6e1f9f
mohamim360 <mohamim360@gmail.com>
2025-03-16 16:26:01 +0600

    add roll method to Dice class for value retrieval

--- Dice.js
+++ Dice.js
@@ -1,10 +1,13 @@
 class Dice {
-	constructor(values) {
-			if (!Array.isArray(values) || values.length < 2) {
-					throw new Error("Dice must have at least two values.");
-			}
-			this.values = values;
-	}
+  constructor(values) {
+    if (!Array.isArray(values) || values.length < 2) {
+      throw new Error("Dice must have at least two values.");
+    }
+    this.values = values;
+  }
+  roll(index) {
+    return this.values[index];
+  }
 }
 
 module.exports = Dice;
